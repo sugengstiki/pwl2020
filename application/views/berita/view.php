30a27f0dab9633f2e443097413b953d953d69df7
sugengstiki <sugeng@stiki.ac.id>
2020-03-12 20:55:47 +0700

    Menggunakan DataTable

--- application/views/berita/view.php
+++ application/views/berita/view.php
@@ -1,6 +1,6 @@
 	<?php
 		//print_r($berita);
-		$template = array('table_open' => '<table>');
+		$template = array('table_open' => '<table id="example">');
 		$this->table->set_template($template);
 		$this->table->set_heading('kode','judul','beritanya','gambarnya','aksi');
 		
@@ -12,5 +12,10 @@
 		echo $this->table->generate();
 	?>
     <p>isi berita</p>
+		<script>
+		$(document).ready(function() {
+				$('#example').DataTable();
+		} );
+		</script>
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Berita</title>
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.css"/>
+ 
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+ 
+		<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.js"></script>
 		
 </head>
 <body>
